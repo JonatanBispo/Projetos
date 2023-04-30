2c5050f4f82cdc32490039f9b5cd41aee91c7b38
Jonatan Bispo <jonatansantos1992@gmail.com>
2023-04-30 15:06:55 -0300

    calculadora internet

---
+++ CALCULADORA - minha/script.js
@@ -0,0 +1,44 @@
+
+function concatenar(numero){
+    let convertido = String(numero)
+
+    let retorno = operador()
+    if (retorno != '') {
+        conc = convertido 
+        
+    }
+    
+     novo =+ conc 
+
+    mostrar(novo)
+
+
+
+
+
+
+
+
+    
+    
+    
+   
+    
+
+}
+
+function operador(operador){
+           alert("operador")
+           let oper
+      return oper     
+
+    }
+
+function mostrar(numero) {
+    visor.innerText = numero
+}
+
+
+function limpar() {
+    visor.innerText = ''
+}
