d7510ecce1fa5dbaf298d6a3ecb605687fe3aa99
Jonatan Bispo <jonatansantos1992@gmail.com>
2023-05-28 14:26:24 -0300

    novo site PET

---
+++ Loja pet/script.js
@@ -0,0 +1,9 @@
+function exibir(){
+    let h = document.getElementById("han")
+    h.style.display = "block"
+   
+}
+function esconder() {
+    let h = document.getElementById("han")
+    h.style.display = "none"
+}
